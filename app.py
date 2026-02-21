a5badadc7f5ae1f97a30409180e1b689d6d1e59b
bropenguin847 <bropenguin847@gmail.com>
2026-02-22 04:55:40 +0800

    Deployment on Render

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
 """
-Main web app ot be run
+Main web app
 
 Initally, the webpage is at index, then when user press submit, it will go to the game
 
-TODO:
-Improve the look of the websites
-
-Add game modes
 """
 
 
 import os
-import gc
 from flask import Flask, render_template, jsonify, session, request
 from game import load_data, grab_info, get_image_url, calculate_delta
 
@@ -36,13 +31,13 @@ def index():
         image_url = get_image_url(ori_code)
         )
 
-@app.route("/pricecatcher")
+@app.route("/pricecatcherTPIR")
 def pricecatcher():
     global result, best_guess, ori_price, item_info
     item_info = grab_info(df_main, df_lookup, df_premise)
 
     return render_template(
-        'pricecatcher.html',
+        'pricecatcherTPIR.html',
         district = item_info['district'],
         state = item_info['state'],
         item_name = item_info['item_name'],
@@ -51,6 +46,21 @@ def pricecatcher():
         ori_price = item_info['price'],
         # result = result,
         # best_guess = session['best_guess']
+    )
+
+@app.route("/pricecatcherDelta")
+def delta():
+    global result, best_guess, ori_price, item_info
+    item_info = grab_info(df_main, df_lookup, df_premise)
+
+    return render_template(
+        'pricecatcherDelta.html',
+        district = item_info['district'],
+        state = item_info['state'],
+        item_name = item_info['item_name'],
+        item_unit = item_info['unit'],
+        image_url = item_info['image_url'],
+        ori_price = item_info['price'],
     )
 
 # Route user to guess price
@@ -65,15 +75,26 @@ def userguess():
     user_guess = float(data.get("user_guess", 0))
     result = float(calculate_delta(ori_price, user_guess))
 
-    if result < float(session['best_guess']):
-        session['best_guess'] = result
+    # "The Price is Right" Logic: Over is a loss
+    if user_guess > ori_price:
+        message = "Bust! You went over the price."
+        status = "lose"
+
+    else:
+        message = f"Great guess! You are RM {result:.2f} away."
+        status = "win"
+
+        if result < float(session['best_guess']):
+            session['best_guess'] = result
 
     # To avoid TypeError, change all values to Python default
     return jsonify({
         'ori_price': ori_price,
         'user_guess': user_guess,
         'result': result,
-        'best_guess': float(session['best_guess'])
+        'best_guess': float(session['best_guess']),
+        'message': message,
+        'status': status
         })
     # if request.method == 'POST':
     #     user_guess = request.form.get('user_guess')
@@ -90,8 +111,8 @@ def userguess():
 
 
 if __name__ == "__main__":
-    # app.run(host="0.0.0.0", port=8000, debug=True)
-    # app.run(debug=True)
     # gunicorn --bind 0.0.0.0:8000 app:app)
+    
+    # For deployment on Render
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port, debug=False)
